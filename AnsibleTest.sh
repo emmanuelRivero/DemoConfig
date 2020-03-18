6ed576f91f357fe1f292d9513f92c6b4852883d5
Emmanuel Rivero <emmanuel@localhost.localdomain>
2020-03-18 11:48:34 -0700

    Test report

--- AnsibleTest.sh
+++ AnsibleTest.sh
@@ -1,6 +1,62 @@
+#!/bin/bash
+### removing results files ###
+rm -f results.txt
+rm -f yml-structure.txt
+rm -f ansible-practice.txt
+
+### running tests ###
+echo Starting YAML-Ansible playbook tests:
+echo YAML auto check results: \n
+
 yamllint .
 if [ $? -gt 0 ]; then
-	echo wrong
+        STRUCTURE=1
 else
-	echo ok
+        STRUCTURE=0
 fi
+
+ansible-lint *.yml *.yaml
+if [ $? -gt 0 ]; then
+        PRACTICE=1
+else
+        PRACTICE=0
+fi
+
+### generating report ###
+echo ============================== >> results.txt
+if [ $STRUCTURE -eq 0 ]; then
+		echo YAML structure: OK >> results.txt
+else
+		echo YAML structure: Fail >> results.txt
+
+        echo YAML structure Test:> yaml-structure.txt
+        echo ============================== >> yaml-structure.txt
+        yamllint . >> yaml-structure.txt
+fi
+if [ $PRACTICE -eq 0 ]; then
+		echo Ansible practice: OK  >> results.txt
+else
+		echo Ansible practice: Fail >> results.txt
+
+        echo Ansible practice Test results: >> ansible-practice.txt
+        echo ============================== >> ansible-practice.txt
+        ansible-lint *.yml *.yaml >> ansible-practice.txt
+fi
+echo ============================== >> results.txt
+
+### priting report to main I/O ###
+echo Test results:
+echo ==============================
+if [ $STRUCTURE -eq 0 ]; then
+		echo YAML structure: OK
+else
+		echo YAML structure: Fail	
+fi
+
+if [ $PRACTICE -eq 0 ]; then
+		echo Ansible practice: OK
+else
+		echo Ansible practice: Fail
+fi
+echo ==============================
+
